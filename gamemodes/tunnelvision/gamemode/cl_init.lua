683a8c206ad43c45bd49467cfe1495a69b52a635
raubana <dylanjraub@yahoo.com>
2018-06-30 20:25:57 -0400

    Added a standard DSP effect. Nice and reverb-ey. Set a standard FOV of55.

--- gamemodes/tunnelvision/gamemode/cl_init.lua
+++ gamemodes/tunnelvision/gamemode/cl_init.lua
@@ -1,1 +1,17 @@
 print( "cl_init" )
+
+
+
+
+function GM:CalcView( ply, origin, angles, fov, znear, zfar )
+	local data = {}
+	data.origin = origin
+	data.angles = angles
+	data.fov = fov
+	data.znear = znear
+	data.zfar = zfar
+	
+	data.fov = 55
+	
+	return data
+end
