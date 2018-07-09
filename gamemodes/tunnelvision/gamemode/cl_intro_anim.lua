f5835d6883e96e10f9a78cd5567d37e9e16738b8
raubana <dylanjraub@yahoo.com>
2018-07-09 14:28:34 -0400

    Updated the intro animation a bit.

--- gamemodes/tunnelvision/gamemode/cl_intro_anim.lua
+++ gamemodes/tunnelvision/gamemode/cl_intro_anim.lua
@@ -8,16 +8,16 @@ local anim_start = 0
 
 local ANIM_LOGO_VISIBLE_DURATION = 32
 local ANIM_HALF_DURATION = 52
-local ANIM_DURATION = 60
+local ANIM_DURATION = 65
 
 
 local CAM_POS_ANIM = TV_ANIM_TRACK:create( {
-	{	t = 0.0,		v = Vector(-167,11958,-231),			i = TV_ANIM_TRACK.INTERP_LINEAR },
-	{	t = 20.0,		v = Vector(1012,11958,-231),			i = TV_ANIM_TRACK.INTERP_EASEOUT_SINE },
-	{	t = 24.0,		v = Vector(1052,11958,-231),			i = TV_ANIM_TRACK.INTERP_EASEIN_SINE },
+	{	t = 0.0,		v = Vector(-167,12860,-231),			i = TV_ANIM_TRACK.INTERP_LINEAR },
+	{	t = 22.5,		v = Vector(1012,12860,-231),			i = TV_ANIM_TRACK.INTERP_EASEOUT_SINE },
+	{	t = 24.0,		v = Vector(1052,12860,-231),			i = TV_ANIM_TRACK.INTERP_EASEIN_SINE },
 	{	t = 32.0,		v = Vector(1052,9216,-231),				i = TV_ANIM_TRACK.INTERP_EASEOUT_SINE },
 	{	t = 44.0,		v = Vector(1153,5713,-431),				i = TV_ANIM_TRACK.INTERP_EASEIN_SINE },
-	{	t = 52.0,		v = Vector(1156,9199,-443) },
+	{	t = 52.0,		v = Vector(1156,6500,-443) },
 	
 }, TV_ANIM_TRACK.OUTPUT_TYPE_VECTOR )
 
@@ -26,7 +26,7 @@ local CAM_ANG_ANIM = TV_ANIM_TRACK:create( {
 	{	t = 24.0,		v = Angle(0,-90,90),					i = TV_ANIM_TRACK.INTERP_SINE },
 	{	t = 31.0,		v = Angle(0,-90,0),						i = TV_ANIM_TRACK.INTERP_EASEIN_SINE },
 	{	t = 32.0,		v = Angle(0,-180,0),					i = TV_ANIM_TRACK.INTERP_EASEOUT_SINE },
-	{	t = 40.0,		v = Angle(0,-270,0),					i = TV_ANIM_TRACK.INTERP_HOLD },
+	{	t = 38.0,		v = Angle(0,-270,0),					i = TV_ANIM_TRACK.INTERP_HOLD },
 	{	t = 52.0,		v = Angle(0,-270,0) },
 }, TV_ANIM_TRACK.OUTPUT_TYPE_ANGLE )
 
@@ -35,6 +35,22 @@ local CAM_FOV_ANIM = TV_ANIM_TRACK:create( {
 	{	t = 24.0,		v = 5,									i = TV_ANIM_TRACK.INTERP_EASEIN_SINE },
 	{	t = 32.0,		v = 45,									i = TV_ANIM_TRACK.INTERP_HOLD },
 	{	t = 32.0,		v = 45 },
+}, TV_ANIM_TRACK.OUTPUT_TYPE_NUMBER )
+
+local CAM_CONTRAST_ANIM = TV_ANIM_TRACK:create( {
+	{	t = 0.0,		v = 100,								i = TV_ANIM_TRACK.INTERP_HOLD },
+	{	t = 32.0,		v = 400,								i = TV_ANIM_TRACK.INTERP_HOLD },
+	{	t = 42.0,		v = 400,								i = TV_ANIM_TRACK.INTERP_EASEIN_SINE },
+	{	t = 52.0,		v = 0,									i = TV_ANIM_TRACK.INTERP_EASEIN_SINE },
+	{	t = 65.0,		v = 100 },
+}, TV_ANIM_TRACK.OUTPUT_TYPE_NUMBER )
+
+local CAM_BLUR_ANIM = TV_ANIM_TRACK:create( {
+	{	t = 0.0,		v = 0,									i = TV_ANIM_TRACK.INTERP_HOLD },
+	{	t = 42.0,		v = 0,									i = TV_ANIM_TRACK.INTERP_EASEIN_SINE },
+	{	t = 52.0,		v = 100,								i = TV_ANIM_TRACK.INTERP_HOLD },
+	{	t = 52.0,		v = 10,									i = TV_ANIM_TRACK.INTERP_SINE },
+	{	t = 65.0,		v = 0 },
 }, TV_ANIM_TRACK.OUTPUT_TYPE_NUMBER )
 
 
@@ -48,7 +64,27 @@ net.Receive( "TV_IntroAnim_Run", function( len )
 	CAM_POS_ANIM:Restart( anim_start )
 	CAM_ANG_ANIM:Restart( anim_start )
 	CAM_FOV_ANIM:Restart( anim_start )
+	CAM_CONTRAST_ANIM:Restart( anim_start )
+	CAM_BLUR_ANIM:Restart( anim_start )
 end )
+
+
+
+
+local blurMat = Material( "pp/videoscale" )
+local function CheapBlur( bluramount )
+	local bluramount = 1.0*bluramount
+
+	while bluramount > 0.25 do
+		blurMat:SetFloat("$scale", bluramount)
+	
+		render.UpdateScreenEffectTexture()
+		render.SetMaterial(blurMat)
+		render.DrawScreenQuad()
+		
+		bluramount = bluramount / 2
+	end
+end
 
 
 
@@ -62,35 +98,27 @@ hook.Add( "RenderScreenspaceEffects", "TV_ClIntroAnim_RenderScreenspaceEffects",
 			net.SendToServer()
 			
 			doing_anim = false
-		elseif t > ANIM_HALF_DURATION then
-			local p = ( t - ANIM_HALF_DURATION ) / ( ANIM_DURATION - ANIM_HALF_DURATION )
-			p = math.sin( math.rad( Lerp( p, 0, 90) ) )
+		end
 		
+		local contrast_acount = CAM_CONTRAST_ANIM:GetOutput()/100
+		if contrast_acount != 1 then
 			local color_mod = {}
 			color_mod["$pp_colour_addr"] = 0
 			color_mod["$pp_colour_addg"] = 0
 			color_mod["$pp_colour_addb"] = 0
 			color_mod["$pp_colour_brightness"] = 0
-			color_mod["$pp_colour_contrast"] = p
+			color_mod["$pp_colour_contrast"] = contrast_acount
 			color_mod["$pp_colour_colour"] = 1
 			color_mod["$pp_colour_mulr"] = 0
 			color_mod["$pp_colour_mulg"] = 0
 			color_mod["$pp_colour_mulb"] = 0
 			
 			DrawColorModify( color_mod )
-		elseif t > ANIM_LOGO_VISIBLE_DURATION then
-			local color_mod = {}
-			color_mod["$pp_colour_addr"] = 0
-			color_mod["$pp_colour_addg"] = 0
-			color_mod["$pp_colour_addb"] = 0
-			color_mod["$pp_colour_brightness"] = 0
-			color_mod["$pp_colour_contrast"] = 4
-			color_mod["$pp_colour_colour"] = 1
-			color_mod["$pp_colour_mulr"] = 0
-			color_mod["$pp_colour_mulg"] = 0
-			color_mod["$pp_colour_mulb"] = 0
-			
-			DrawColorModify( color_mod )
+		end
+		
+		local blur_amount = CAM_BLUR_ANIM:GetOutput()/100
+		if blur_amount > 0 then
+			CheapBlur( blur_amount*ScrH()*0.1 )
 		end
 		
 		if anim_prev_t < ANIM_HALF_DURATION and t >= ANIM_HALF_DURATION then
@@ -122,6 +150,9 @@ hook.Add( "CalcView", "TV_ClIntroAnim_CalcView", function( ply, origin, angles, 
 		data.zfar = zfar
 		
 		local t = RealTime()
+		
+		CAM_BLUR_ANIM:Update( t )
+		CAM_CONTRAST_ANIM:Update( t )
 		
 		if t < anim_start + ANIM_HALF_DURATION then
 		
