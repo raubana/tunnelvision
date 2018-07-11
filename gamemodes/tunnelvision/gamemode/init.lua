33218b2ecb3ac7e67299cd404ef9c2d9a0036977
raubana <dylanjraub@yahoo.com>
2018-07-11 08:08:42 -0400

    Removed the mapgen stuff (it's not going to be used anyways).

--- gamemodes/tunnelvision/gamemode/init.lua
+++ gamemodes/tunnelvision/gamemode/init.lua
@@ -3,7 +3,6 @@ AddCSLuaFile( "tv_anim_track.lua" )
 
 include( "shared.lua" )
 include( "sv_intro_anim.lua" )
-include( "sv_mapgen.lua" )
 
 
 
@@ -30,75 +29,6 @@ end
 
 
 
-function GM:SpawnHim( pos )
-	print( "HE LIVES" )
-
-	local ent = ents.Create("snpc_weeping_gman")
-	ent:SetPos( pos )
-	ent:SetAngles( Angle(0, math.random()*360, 0) )
-	ent:Spawn()
-	ent:Activate()
-end
-
-
-
-
-function GM:SpawnFlies()
-	navs = navmesh.GetAllNavAreas()
-	
-	for x = 1, math.ceil(#navs*0.1) do
-		local nav = navs[math.random(#navs)]
-	
-		if nav and IsValid(nav) then
-			local ent = ents.Create( "sent_tv_fly" )
-			ent:SetPos( nav:GetCenter() )
-			ent:SetAngles( Angle(0, math.random()*360, 0) )
-			ent:Spawn()
-			ent:Activate()
-		end
-	end
-end
-
-
-
-
-function GM:SpawnKey( pos, door_name, index )
-	local color = DOOR_CONNECTION_TABLE[door_name][3]
-
-	local ent_list = ents.FindByName( door_name )
-	local door = ent_list[1]
-	door:SetColor( color )
-	
-	local ent = ents.Create( "sent_tv_key" )
-	ent:SetPos( pos )
-	ent:SetAngles( AngleRand() )
-	ent:SetColor( color )
-	ent:Spawn()
-	ent:Activate()
-	
-	ent.door = door
-end
-
-
-
-
-function GM:SpawnCorpse( pos )
-	local ent = ents.Create( "sent_tv_living_corpse" )
-	ent:SetPos( pos )
-	ent:Spawn()
-	ent:Activate()
-	
-	for i = 1, 25 do
-		local fly = ents.Create( "sent_tv_fly" )
-		fly:SetPos( pos )
-		fly:Spawn()
-		fly:Activate()
-	end
-end
-
-
-
-
 function GM:PlayerNoClip( ply, desiredState )
 	return not desiredState or GetConVar( "sv_cheats" ):GetBool()
 end
@@ -109,32 +39,6 @@ end
 function GM:PlayerInitialSpawn( ply )
 	if not game.SinglePlayer() then
 		ply:Kick( "this is a singleplayer gamemode, dummy" )
-	end
-	
-	local result = self:GenerateMap()
-	
-	self.player_spawn = nil
-	
-	if istable( result ) then
-	
-		PrintTable( result )
-		
-		for i, data in ipairs( result.spawns ) do
-			if isstring( data ) then
-				if data == "player" then
-					self.player_spawn = SPAWN_LOCATION_TABLE[i][2]
-				elseif data == "gman" then
-					self:SpawnHim( SPAWN_LOCATION_TABLE[i][2] )
-				elseif data == "corpse" then
-					self:SpawnCorpse( SPAWN_LOCATION_TABLE[i][2] )
-				end
-			elseif istable( data ) then
-				self:SpawnKey( SPAWN_LOCATION_TABLE[i][2], data[2], data[1] )
-			end
-		end
-		
-		self:SpawnFlies()
-		
 	end
 end
 
